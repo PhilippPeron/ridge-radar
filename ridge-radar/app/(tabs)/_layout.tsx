6fe3f45b0a6c7232efbfcfc5f9317dedd0e91e19
Philipp Peron <philipp.peron@gmail.com>
2024-11-29 01:29:31 +0100

    Add correct tabs

--- ridge-radar/app/(tabs)/_layout.tsx
+++ ridge-radar/app/(tabs)/_layout.tsx
@@ -1,28 +1,54 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
+import { View, TouchableWithoutFeedbackComponent } from "react-native";
+import WeatherIcon from "../../assets/icons/weather.svg";
+import BellIcon from "../../assets/icons/bell.svg";
+import SettingsIcon from "../../assets/icons/settings.svg";
+
+import tailwindConfig from "../../tailwind.config.js";
+const tailwindColors = tailwindConfig.theme.extend.colors;
+const iconSize = 30;
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: tailwindColors.text,
+        tabBarInactiveTintColor: tailwindColors.secondary,
+        tabBarStyle:  { position: 'absolute', height: 70, paddingBottom: 0},
+        tabBarLabelPosition: "beside-icon",
+        tabBarShowLabel: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Weather',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cloud" color={color} />,
+          title: "Weather",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+              <WeatherIcon height={iconSize+5} width={iconSize+5} fill={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="notifications"
         options={{
-          title: 'Notifications',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bell" color={color} />,
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <View>
+              <BellIcon height={iconSize} width={iconSize} fill={color} />
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <View>
+              <SettingsIcon height={iconSize} width={iconSize} fill={color} />
+            </View>
+          ),
         }}
       />
     </Tabs>
--- ridge-radar/app/(tabs)/notifications.tsx
+++ ridge-radar/app/(tabs)/notifications.tsx
@@ -1,9 +1,12 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import Logo from '../../assets/icons/pencil.svg';
 
 export default function Tab() {
   return (
     <View style={styles.container}>
       <Text className="font-rblack">Tab Notifications</Text>
+      <Logo width={100} height={100} fill={"#000"}/>
+      <StatusBar translucent={true}/>
     </View>
   );
 }
